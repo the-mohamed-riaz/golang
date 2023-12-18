9b0c8fbe9acbf5b9817f18416a19386392cb54d2
Riaz <the.mohamed.riaz@gmail.com>
2023-12-18 12:01:22 +0530

    Channel as queue

--- channel/channel.go
+++ channel/channel.go
@@ -2,6 +2,35 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("hello world")
+type Que struct {
+	items chan int
 }
+
+func (q *Que) Enque(item int) {
+	q.items <- item
+}
+
+func (q *Que) Deque() int {
+	return <-q.items
+}
+
+func main() {
+	fmt.Println("start")
+	person := []int{1, 2, 3, 4, 5}
+	q := Que{
+		items: make(chan int, len(person)),
+	}
+
+	for _, n := range person {
+		q.Enque(n)
+		fmt.Print("in Que: ", n, " ")
+		fmt.Println("Deque: ", q.Deque())
+	}
+	// fmt.Println("Deque: ", q.Deque())
+}
+
+// NOTE
+// ERROR: fatal error: all goroutines are asleep - deadlock!
+// You will get this error, when you are trying to over-read from the channel
+// over-read? expecting an input from the channel, when it has nothing.
+// uncomment the extra deque print statement to observe the error.
